0f9348c7d99645facf5a26ec6d55e6a023f2df5c
Chongzi <111750846+chong1010@users.noreply.github.com>
2026-09-23 01:32:27 +0800

    Update NumericApplicationTests.java

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -1,35 +1,35 @@
 package com.devsecops;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class NumericApplicationTests {
+class NumericApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
     @Test
-    public void welcomeTest() throws Exception {
+    void welcomeTest() throws Exception {
         this.mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Kubernetes DevSecOps"));
     }
 
     @Test
-    public void compareToFiftyTest() throws Exception {
+    void compareToFiftyTest() throws Exception {
         this.mockMvc.perform(get("/compare/55"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Greater than 50"));
